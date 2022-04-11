3292b38ac029fb2e59ad328fd5ca5a2db2cd8aed
Ryan Tiffany <ryantiffany@fastmail.com>
2022-04-11 12:36:09 -0500

    Added in resource group example

--- get-resource-groups.py
+++ get-resource-groups.py
@@ -13,8 +13,30 @@ resource_group = os.environ.get('RESOURCE_GROUP')
 service = ResourceManagerV2(authenticator=authenticator)
 
 resource_group_list = service.list_resource_groups(
-  account_id=(os.environ.get('IBMCLOUD_ACCOUNT_ID')),
-  include_deleted=True,
-).get_result()['id']
+  account_id=(os.environ.get('IBM_ACCOUNT')),
+  include_deleted=False,
+).get_result()
 
-print(resource_group_list)
+pprint("Listing Account Resource Groups:")
+for rg in resource_group_list['resources']:
+   print( rg['id'], "\t",  rg['name'])
+
+
+print(json.dumps(resource_group_list))
+
+
+
+data=[]
+for rg in resource_group_list['resources']:
+    data.append({
+        "name": rg.get('name'),
+        "id": rg.get('id'),
+        "crn": rg.get('crn')
+    })
+
+pprint(data)
+# print("Getting details of env rg")
+
+# resource_group_get = service.get_resource_group(
+#   id=id
+# ).get_result()
